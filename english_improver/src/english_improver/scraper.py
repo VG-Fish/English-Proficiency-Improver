8bc79be5cb274307d64471e382ef587c589dd472
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-05-21 18:20:12 -0500

    removed import statement.

--- english_improver/src/english_improver/scraper.py
+++ english_improver/src/english_improver/scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 from typing import List, Self, Dict
 from random import sample
-from asyncio import gather, run
+from asyncio import gather
 
 
 class GutenbergScraper:
